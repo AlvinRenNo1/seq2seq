0d4858e9bf0ee5c4a52089d3e304b589b27c5250
AlvinRenNo1 <renqingmail@126.com>
2018-03-23 05:26:31 +0000

    enable saving model

--- seq2seq.py
+++ seq2seq.py
@@ -825,7 +825,8 @@ evaluateRandomly(encoder1, attn_decoder1)
 output_words, attentions = evaluate(
     encoder1, attn_decoder1, "mir geht es gut .")
 #plt.matshow(attentions.numpy())
-
+torch.save(encoder1.state_dict(), 'encoder1.params.pkl')
+torch.save(attn_decoder1.state_dict(), 'attn_decoder1.params.pkl')
 
 ######################################################################
 # For a better viewing experience we will do the extra work of adding axes
